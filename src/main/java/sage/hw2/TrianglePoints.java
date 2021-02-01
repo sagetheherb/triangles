9446fdffd2ca1ff36bdda95f1ca4bd4ff503cfa6
sagetheherb <sage.elfanbaum@gmail..com>
2021-02-01 17:37:14 -0600

    top comment

---
+++ src/main/java/sage/hw2/TrianglePoints.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sage.hw2;
+
+/**
+ * Triangle Points 
+ * hw2
+ * Java NetBeans IDE 12.2
+ * @author Sage Elfanbaum and Erosh Boralugodage
+ * CS4500 section 1
+ * 2/8/2021
+ * This program will prompt the user for three points that form a 
+ * triangle and if it is not colinear it will calculate the triangle's
+ * centroid, orthocenter and centroid. The program will then calculate 
+ * the value of the biggest distance between the points. Then two of 
+ * the points will be used to create a line that pass through them. 
+ * Then the third point's distance to that line will be mesured. 
+ * TODO enter resorces used to create program
+ */
+
+public class TrianglePoints {
+    
+     public static void main(String[] args) {
+        
+    }
+  
+}
