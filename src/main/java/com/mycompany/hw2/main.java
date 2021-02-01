49af932afa234a29a5307e5eec98050ee0530cff
sagetheherb <sage.elfanbaum@gmail..com>
2021-02-01 17:20:15 -0600

    main

---
+++ src/main/java/com/mycompany/hw2/main.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.hw2;
+
+/**
+ *
+ * @author sagee
+ */
+public class main {
+    
+}
