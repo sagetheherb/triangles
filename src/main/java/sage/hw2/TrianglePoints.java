0a589904208f730751cda63d458c6503f43a949d
sagetheherb <sage.elfanbaum@gmail..com>
2021-02-04 10:05:03 -0600

    orthocenter

--- src/main/java/sage/hw2/TrianglePoints.java
+++ src/main/java/sage/hw2/TrianglePoints.java
@@ -28,12 +28,12 @@ import java.util.ArrayList;
 public class TrianglePoints {
     
      public static void main(String[] args) {
-        System.out.println("This program will prompt the user for three points that form a "
-        + "triangle and if it is not colinear it will calculate the triangle"
-        + "centroid, orthocenter and centroid. The program will then calculate "
-        + "the value of the biggest distance between the points. Then two of "
-        + "the points will be used to create a line that pass through them. "
-        + "Then the third point distance to that line will be measured."); 
+        System.out.println("This program will prompt the user for three points that form a ");
+        System.out.println( "triangle and if it is not colinear it will calculate the triangle");
+        System.out.println( "centroid, orthocenter and centroid. The program will then calculate ");
+        System.out.println( "the value of the biggest distance between the points. Then two of ");
+        System.out.println( "the points will be used to create a line that pass through them. ");
+        System.out.println( "Then the third point distance to that line will be measured."); 
         
         Scanner in = new Scanner(System.in);
         
@@ -47,23 +47,26 @@ public class TrianglePoints {
             double tempY = in.nextDouble();
             
             Point point = new Point(tempX, tempY);
+            System.out.print(point.getX() + "x value");
+            System.out.print(point.getY() + "y value");
             points.add(point);
             point_counter++;
             if(point_counter >= 3){
                 break;
             }
          
-      
+        }
         Triangle triangle = new Triangle(points.get(0), points.get(1), points.get(2));
-        if (triangle.isColinear()){
+        if (triangle.isCollinear()){
             System.out.print("the points you have entered are colinear sorry");
             in.nextLine();
             System.exit(0);
         }
-        /**
-         * to do print out the three points 
-         */
-        }
+
+        System.out.print(triangle.getOrthocenter().getX() + "," + triangle.getOrthocenter().getY() + " is the orthocenter");
+        in.nextLine();
+        System.exit(0);
+        
        
        
        
