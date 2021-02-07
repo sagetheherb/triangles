95dbe29875af03cd4acd0fa6676a55ed63fdff9a
Erosh <erosh.bc@gmail.com>
2021-02-07 02:21:50 -0600

    calculate centeroid and calculateCircumcenter added to the file

--- src/main/java/sage/hw2/TrianglePoints.java
+++ src/main/java/sage/hw2/TrianglePoints.java
@@ -2,41 +2,42 @@
 package sage.hw2;
 
 /**
- * Triangle Points 
+ * Triangle Points
  * hw2
  * Java NetBeans IDE 12.2
+ *
  * @author Sage Elfanbaum and Erosh Boralugodage
  * CS4500 section 1
  * 2/8/2021
- * This program will prompt the user for three points that form a 
+ * This program will prompt the user for three points that form a
  * triangle and if it is not colinear it will calculate the triangle's
- * centroid, orthocenter and centroid. The program will then calculate 
- * the value of the biggest distance between the points. Then two of 
- * the points will be used to create a line that pass through them. 
- * Then the third point's distance to that line will be measured. 
+ * centroid, orthocenter and centroid. The program will then calculate
+ * the value of the biggest distance between the points. Then two of
+ * the points will be used to create a line that pass through them.
+ * Then the third point's distance to that line will be measured.
  * TODO enter resources used to create program
  */
 
 import java.util.Scanner;
-import java.util.ArrayList; 
-
+import java.util.ArrayList;
+import sage.*
 
 public class TrianglePoints {
-    
-     public static void main(String[] args) {
+
+    public static void main(String[] args) {
         System.out.println("This program will prompt the user for three points that form a ");
-        System.out.println( "triangle and if it is not colinear it will calculate the triangle");
-        System.out.println( "centroid, orthocenter and centroid. The program will then calculate ");
-        System.out.println( "the value of the biggest distance between the points. Then two of ");
-        System.out.println( "the points will be used to create a line that pass through them. ");
-        System.out.println( "Then the third point distance to that line will be measured."); 
-        
+        System.out.println("triangle and if it is not colinear it will calculate the triangle");
+        System.out.println("centroid, orthocenter and centroid. The program will then calculate ");
+        System.out.println("the value of the biggest distance between the points. Then two of ");
+        System.out.println("the points will be used to create a line that pass through them. ");
+        System.out.println("Then the third point distance to that line will be measured.");
+
         Scanner in = new Scanner(System.in);
-        
+
         ArrayList<Point> points = new ArrayList<>();
         int point_counter = 0;
         //this loop takes in three points from the user
-        while (true){
+        while (true) {
             try {
                 double tempX = 0.0;
                 double tempY = 0.0;
@@ -49,38 +50,34 @@ public class TrianglePoints {
                 System.out.println(point.getY() + " y value");
                 points.add(point);
                 point_counter++;
-                
-            }
-            
-            catch(Exception InputMismatchException) {
+
+            } catch (Exception InputMismatchException) {
                 System.out.println(" please enter a decimal number i.e. 1.2");
                 in.next();
             }
-            
-        if(point_counter >= 3){
-            break;
+
+            if (point_counter >= 3) {
+                break;
+            }
+
         }
-         
-        }
-        
+
         Triangle triangle = new Triangle(points.get(0), points.get(1), points.get(2));
-        if (triangle.isCollinear()){
+        if (triangle.isCollinear()) {
             System.out.print("the points you have entered are colinear sorry");
             in.nextLine();
             System.exit(0);
         }
 
         System.out.print(triangle.getOrthocenter().getX() + "," + triangle.getOrthocenter().getY() + " is the orthocenter");
-        //System.out.print(triangle.getCircumventer().getX() + "," + triangle.getCircumventer().getY() + " is the circumventer");
-        //System.out.print(triangle.getCentroid().getX() + "," + triangle.getCentroid().getY() + " is the centroid");
-        //System.out.print(triangle.getEulerLine());
+        System.out.print(triangle.getCircumventer().getX() + "," + triangle.getCircumventer().getY() + " is the circumventer");
+       System.out.print(triangle.getCentroid().getX() + "," + triangle.getCentroid().getY() + " is the centroid");
+        System.out.print(triangle.getEulerLine());
         in.nextLine();
         System.exit(0);
-        
-       
-       
-       
+
+
     }
-  
-  
+
+
 }
--- src/main/java/sage/hw2/triangle.java
+++ src/main/java/sage/hw2/triangle.java
@@ -6,35 +6,137 @@
 package sage.hw2;
 
 /**
- *
  * @author sagee
  */
-public class triangle {
-    private point point1;
-    private point point2;
-    private point point3;
-    
+public class Triangle {
+    private Point pointA;
+    private Point pointB;
+    private Point pointC;
+    private Point orthocenter;
+    private Point centroid;
+    private Point circumventer;
 
-    public triangle(point point1, point point2, point point3) {
-        
-        this.point1 = point1;
-        this.point2 = point2;
-        this.point3 = point3;
-        
+
+    public Triangle(Point pointA, Point pointB, Point pointC) {
+
+        this.pointA = pointA;
+        this.pointB = pointB;
+        this.pointC = pointC;
+        calculateOrthocenter();
+        calculateCentroid();
+        calculateCircumventer();
+
+    }
+//Centroid of the triange is x= (x1 + x2 + x3)/3 and y= (y1+y2+y3)/3
+    private void calculateCentroid() {
+        double x = (pointA.getX() + pointB.getX()+pointC.getX())/3;
+        double y =(pointA.getY() + pointB.get() + pointC.getY())/3;
+        Point point = new Point(x, y);
+        this.orthocenter = point;
     }
 
-    public point getPoint1() {
-        return point1;
+
+    // This function Perpendicular bisector slope
+    private void calculateCircumventer() {
+        //calculate slope from point to point from triangle
+        //slope  = y2-y1/x2-x1
+        double slopeBC = (pointC.getY() - pointB.getY()) / (pointC.getX() - pointB.getX()); //slope BC
+        double slopeAC = (pointC.getY() - pointA.getY()) / (pointC.getX() - pointA.getX()); //Slope AC
+
+
+        //slope of the line that contain the perpendicular bi sector passes through the mid point
+        double slopePerpBiSecBC = -1 / slopeBC;
+        double slopePerpBiSecAC = -1 / slopeAC;
+
+        //calculate mid point from point to point BC
+        double midPointBCX = (pointB.getX() +pointC.getX())/2;
+        double midPointBCY = (pointB.getY() +pointC.getY())/2;
+
+        //calculate mid point from point to point AC
+        double midPointACX = (pointA.getX() +pointC.getX())/2;
+        double midPointACY = (pointA.getY() +pointC.getY())/2;
+
+
+// equation for perpendicular bisector going through mid point of side1 (x1,y1) and circumcenter (X,Y)
+//  (Y-y1) = m1(X-x1)
+//   Y = m1(X-x1) + Y1
+
+// equation flor perpendicular bisector going through mid point of side2 (x2,y2) and circumcenter (X,Y)
+//  (Y-y2) = m1(X-x2)
+//   Y = m2(X-x2) + Y2
+
+//find the point of intersection of two lines
+// m1(X-x1) + y1  = m2(X-x2) + y2
+//X = (m1*x1-m2*x2 +Y2-y1)/(m1-m2)
+// Y = m1*(X-x1)+Y1
+
+        if (m1 == m2) {
+            System.out.println("Parallel lines from the Perpendicular bi sector from mid points");
+        } else {
+
+            double x = (slopePerpBiSecAC * midPointACX - slopePerpBiSecBC * midPointBCX + midPointBCY - midPointACY) / (slopePerpBiSecAC - slopePerpBiSecBC);
+            double y = slopePerpBiSecAC * (x - midPointACX) + midPointACY;
+
+            Point.point = new Point(x,y);
+            this.circumventer = point;
+        }
+
+
+}
+
+
+    private void calculateOrthocenter() {
+
+        double slopeBC = (pointC.getY() - pointB.getY()) / (pointC.getX() - pointB.getX());
+        double slopeAC = (pointC.getY() - pointA.getY()) / (pointC.getX() - pointA.getX());
+        double slopeAB = (pointB.getY() - pointA.getY()) / (pointB.getX() - pointA.getX());
+
+        double slopeAD = -1.0 / slopeBC; // -1/slope = perpendicular
+        double slopeCF = -1.0 / slopeAB;
+
+        double yInterceptAD = (pointA.getY() - (slopeAD * pointA.getX())); // b = y -mx
+        double yInterceptCF = (pointC.getY() - (slopeCF * pointC.getX())); // b = y -mx
+
+
+        double x = (yInterceptCF - yInterceptAD) / (slopeAD - slopeCF);
+        double y = slopeAD * x + yInterceptAD;
+
+        Point point = new Point(x, y);
+        this.orthocenter = point;
+
+
     }
 
-    public point getPoint2() {
-        return point2;
+    public Point getOrthocenter() {
+
+        return this.orthocenter;
+
     }
 
-    public point getPoint3() {
-        return point3;
+    public Point getCentroid() {
+
+        return this.centroid;
+
     }
 
-   
-    
+    public Point getCircumventer() {
+
+        return this.circumventer;
+    }
+
+    public boolean isCollinear() {
+        return false;
+
+    }
+
+    public boolean isEquilateral() {
+        return (this.centroid.getX() == this.circumventer.getX()
+                && this.circumventer.getX() == this.orthocenter.getX()
+                && this.centroid.getY() == this.circumventer.getY()
+                && this.circumventer.getY() == this.orthocenter.getY());
+
+
+    }
+
+
 }
